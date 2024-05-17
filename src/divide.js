3d8c621007f3dd89f5ce74712904d5f03e52434a
Mark Landers <alexandre@dashlane.com>
2024-05-17 15:13:48 +0200

    fix

--- src/divide.js
+++ src/divide.js
@@ -2,8 +2,5 @@ function divide(numOne, numTwo) {
   if (numOne == undefined || numTwo == undefined) {
     return undefined;
   }
-  if (typeof numOne !== "number" || typeof numTwo !== "number") {
-    return undefined;
-  }
   return numOne / numTwo;
 }
